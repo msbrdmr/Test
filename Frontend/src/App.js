8e3a0f9d26c33d43a2d667e545d94f185f4ef0a1
Sselimkoc <selokoc.1@gmail.com>
2023-11-29 19:46:25 +0300

    add login page on both back and frontend

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,23 +2,24 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import * as pages from "./Pages";
-
+import HomeLogin from "./pages/HomeLogin";
 function App() {
-	return (
-		<Router>
-			{/* <Dashboard /> */}
-			<Routes>
-				<Route path="/" element={<Dashboard />}>
-					<Route index element={<pages.Profile />} />
-					<Route path="/profile" element={<pages.Profile />} />
-					<Route path="/forms" element={<pages.Forms />} />
-					<Route path="/signin" element={<pages.SignIn />} />
-					<Route path="/signup" element={<pages.SignUp />} />
-					<Route path="/settings" element={<pages.Settings />} />
-				</Route>
-			</Routes>
-		</Router>
-	);
+  return (
+    <Router>
+      {/* <Dashboard /> */}
+      <Routes>
+        <Route path="/" element={<Dashboard />}>
+          <Route index element={<pages.Profile />} />
+          <Route path="/profile" element={<pages.Profile />} />
+          <Route path="/forms" element={<pages.Forms />} />
+		  <Route path="/login" element={<HomeLogin></HomeLogin>}></Route>
+          <Route path="/signin" element={<pages.SignIn />} />
+          <Route path="/signup" element={<pages.SignUp />} />
+          <Route path="/settings" element={<pages.Settings />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- Frontend/src/Dashboard.js
+++ Frontend/src/Dashboard.js
@@ -1,76 +1,131 @@
 import React, { useState } from "react";
 import { Layout, Menu } from "antd";
-import { UserOutlined, FormOutlined, LoginOutlined, LogoutOutlined, SettingOutlined, MenuFoldOutlined } from "@ant-design/icons";
+import {
+  UserOutlined,
+  FormOutlined,
+  LoginOutlined,
+  LogoutOutlined,
+  SettingOutlined,
+  MenuFoldOutlined,
+} from "@ant-design/icons";
 import { Link, Outlet } from "react-router-dom";
 const menuItemFoldStyle = {
-	display: "flex",
-	alignItems: "center",
-	justifyContent: "center",
-	fontSize: 0, // Set font size to 0 to make text invisible
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  fontSize: 0, // Set font size to 0 to make text invisible
 };
 const menuItemUnFoldStyle = {
-	// Adjusted to match the width of the Sider in Dashboard.js
-	padding: "0px 0px 0px 10px ",
+  // Adjusted to match the width of the Sider in Dashboard.js
+  padding: "0px 0px 0px 10px ",
 };
 const { Sider, Content, Header } = Layout;
 const UnfoldNavbarWidth = 150;
 const FoldNavbarWidth = 40;
 const Dashboard = () => {
-	const [fold, setFold] = useState(false);
-	const [navbarWidth, setNavbarWidth] = useState(UnfoldNavbarWidth);
+  const [fold, setFold] = useState(false);
+  const [navbarWidth, setNavbarWidth] = useState(UnfoldNavbarWidth);
 
-	const changeFold = () => {
-		setFold(!fold);
-		fold ? setNavbarWidth(UnfoldNavbarWidth) : setNavbarWidth(FoldNavbarWidth);
-	};
-	return (
-		<>
-			<Layout style={{ minHeight: "100vh" }}>
-				<Header style={{borderRadius:"10px 10px 0px 0px", display: "flex", alignItems: "center", backgroundColor:"#0f0f11"}}>
-					<Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]} />
-				</Header>
+  const changeFold = () => {
+    setFold(!fold);
+    fold ? setNavbarWidth(UnfoldNavbarWidth) : setNavbarWidth(FoldNavbarWidth);
+  };
+  return (
+    <>
+      <Layout style={{ minHeight: "100vh" }}>
+        <Header
+          style={{
+            borderRadius: "10px 10px 0px 0px",
+            display: "flex",
+            alignItems: "center",
+            backgroundColor: "#0f0f11",
+          }}
+        >
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]} />
+        </Header>
 
-				<Layout>
-					<Sider style={{ border: "2px solid black" }} width={navbarWidth} theme="light">
-						<Menu mode="vertical" theme="light" defaultSelectedKeys={["1"]} style={{ display: "flex", flexDirection: "column", justifyContent: "space-between", height: "100%" }}>
-							<Menu.Item style={fold ? menuItemFoldStyle : menuItemUnFoldStyle} key="1" icon={<UserOutlined />}>
-								<Link to="/profile">Profile</Link>
-							</Menu.Item>
-							<Menu.Item style={fold ? menuItemFoldStyle : menuItemUnFoldStyle} key="2" icon={<FormOutlined />}>
-								<Link to="/forms">Forms</Link>
-							</Menu.Item>
-							<Menu.Item style={fold ? menuItemFoldStyle : menuItemUnFoldStyle} key="3" icon={<LoginOutlined />}>
-								<Link to="/signin">Sign In</Link>
-							</Menu.Item>
-							<Menu.Item style={fold ? menuItemFoldStyle : menuItemUnFoldStyle} key="4" icon={<LogoutOutlined />}>
-								<Link to="/signup">Sign Up</Link>
-							</Menu.Item>
-							<Menu.Item style={fold ? menuItemFoldStyle : menuItemUnFoldStyle} key="5" icon={<SettingOutlined />}>
-								<Link to="/settings">Settings</Link>
-							</Menu.Item>
-							<Menu.Item
-								onClick={changeFold}
-								key="6"
-								icon={<MenuFoldOutlined />}
-								style={{
-									marginTop: "auto",
-									alignItems: "center",
-									justifyContent: "center"
-								}}
-							></Menu.Item>
-						</Menu>
-					</Sider>
-					<Layout>
-						<Content style={{ display: "flex", flexDirection: "column", padding: "16px", border: "1px solid red" }}>
-							<div>
-								<Outlet />
-							</div>
-						</Content>
-					</Layout>
-				</Layout>
-			</Layout>
-		</>
-	);
+        <Layout>
+          <Sider
+            style={{ border: "2px solid black" }}
+            width={navbarWidth}
+            theme="light"
+          >
+            <Menu
+              mode="vertical"
+              theme="light"
+              defaultSelectedKeys={["1"]}
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+                height: "100%",
+              }}
+            >
+              <Menu.Item
+                style={fold ? menuItemFoldStyle : menuItemUnFoldStyle}
+                key="1"
+                icon={<UserOutlined />}
+              >
+                <Link to="/profile">Profile</Link>
+              </Menu.Item>
+              <Menu.Item
+                style={fold ? menuItemFoldStyle : menuItemUnFoldStyle}
+                key="2"
+                icon={<FormOutlined />}
+              >
+                <Link to="/forms">Forms</Link>
+              </Menu.Item>
+              <Menu.Item
+                style={fold ? menuItemFoldStyle : menuItemUnFoldStyle}
+                key="3"
+                icon={<LoginOutlined />}
+              >
+                <Link to="/login">Sign In</Link>
+              </Menu.Item>
+              <Menu.Item
+                style={fold ? menuItemFoldStyle : menuItemUnFoldStyle}
+                key="4"
+                icon={<LogoutOutlined />}
+              >
+                <Link to="/signup">Sign Up</Link>
+              </Menu.Item>
+              <Menu.Item
+                style={fold ? menuItemFoldStyle : menuItemUnFoldStyle}
+                key="5"
+                icon={<SettingOutlined />}
+              >
+                <Link to="/settings">Settings</Link>
+              </Menu.Item>
+              <Menu.Item
+                onClick={changeFold}
+                key="6"
+                icon={<MenuFoldOutlined />}
+                style={{
+                  marginTop: "auto",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              ></Menu.Item>
+            </Menu>
+          </Sider>
+          <Layout>
+            <Content
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                padding: "16px",
+                border: "1px solid red",
+              }}
+            >
+              <div>
+                <Outlet />
+              </div>
+            </Content>
+          </Layout>
+        </Layout>
+      </Layout>
+    </>
+  );
 };
 
 export default Dashboard;
