1b27a8d17322b994658c480568fa499e1a74389b
Musab Erdemir <musaberdemirmc2@gmail.com>
2023-11-30 19:48:18 +0300

    Update Dashboard.js

--- Frontend/src/Dashboard.js
+++ Frontend/src/Dashboard.js
@@ -20,10 +20,16 @@ const menuItemUnFoldStyle = {
 const { Sider, Content, Header } = Layout;
 const UnfoldNavbarWidth = 150;
 const FoldNavbarWidth = 40;
+
 const Dashboard = ({ darkMode, handleDarkModeChange }) => {
 	const [fold, setFold] = useState(true);
 	const [navbarWidth, setNavbarWidth] = useState(FoldNavbarWidth);
+	const menuStyle = {
+		background: darkMode ? "linear-gradient(to right bottom, #3c4149, #464b52, #51555c, #5b5f66, #666970)" : "white",
+		color: darkMode ? "white" : "black",
+	};
 
+	
 	const handleMouseEnter = () => {
 		setFold(false);
 		setNavbarWidth(UnfoldNavbarWidth);
@@ -36,7 +42,7 @@ const Dashboard = ({ darkMode, handleDarkModeChange }) => {
 	return (
 		<div>
 			<Layout style={{ minHeight: "80vh" }}>
-				<Header style={{ background: darkMode ? "#001529" : "#FFF", color: darkMode ? "#FFF" : "#001529" }}>
+				<Header style={{ background: darkMode ? "linear-gradient(to right top, #3c4149, #464b52, #51555c, #5b5f66, #666970)" : "#FFF", color: darkMode ? "#FFF" : "#001529" }}>
 					<div style={{ marginRight: "auto" }}>
 						<img src="https://i.imgur.com/MgnwjM9.png" alt="Logo" style={{ marginTop: "25px", height: "68px", width: "auto" }} />
 					</div>
@@ -45,20 +51,20 @@ const Dashboard = ({ darkMode, handleDarkModeChange }) => {
 
 					<Switch checked={darkMode} onChange={handleDarkModeChange} style={{ marginLeft: "10px", marginRight: "10px" }} />
 
-
 					<ProfileButtonArea userName="John Doe" imageUrl="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
 				</Header>
 
 				<Layout>
-					<Sider  width={navbarWidth} theme="light" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+					<Sider width={navbarWidth} theme="light" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
 						<Menu
 							mode="vertical"
-							theme={darkMode ? "dark" : "light"}  
+							theme={darkMode ? "dark" : "light"}
 							defaultSelectedKeys={["1"]}
 							style={{
 								display: "flex",
 								flexDirection: "column",
 								height: "100%",
+								...menuStyle,
 							}}
 						>
 							<Menu.Item style={fold ? menuItemFoldStyle : menuItemUnFoldStyle} key="1" icon={<UserOutlined />}>
